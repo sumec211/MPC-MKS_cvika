3da80d167df65152a19059fc0d162fa48c9a8215
Stanislav Sindler <sumec211@seznam.cz>
2021-09-30 07:27:41 +0200

    zprava

---
+++ Cviceni2/Debug/Src/main.d
@@ -0,0 +1,18 @@
+Src/main.o: ../Src/main.c \
+ ../CMSIS/Device/ST/STM32F0xx/Include/stm32f0xx.h \
+ ../CMSIS/Device/ST/STM32F0xx/Include/stm32f030x8.h \
+ ../CMSIS/Include/core_cm0.h ../CMSIS/Include/core_cmInstr.h \
+ ../CMSIS/Include/core_cmFunc.h \
+ ../CMSIS/Device/ST/STM32F0xx/Include/system_stm32f0xx.h
+
+../CMSIS/Device/ST/STM32F0xx/Include/stm32f0xx.h:
+
+../CMSIS/Device/ST/STM32F0xx/Include/stm32f030x8.h:
+
+../CMSIS/Include/core_cm0.h:
+
+../CMSIS/Include/core_cmInstr.h:
+
+../CMSIS/Include/core_cmFunc.h:
+
+../CMSIS/Device/ST/STM32F0xx/Include/system_stm32f0xx.h:
