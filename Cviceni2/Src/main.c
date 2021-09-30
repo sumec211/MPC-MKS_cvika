6d6bd30bb7506f242a4026b83e42e6bacde13354
Stanislav Sindler <sumec211@seznam.cz>
2021-09-30 09:22:13 +0200

    Po splneni ukolu c.3

--- Cviceni2/Src/main.c
+++ Cviceni2/Src/main.c
@@ -24,56 +24,113 @@
   #warning "FPU is not initialized, but the project is compiling for an FPU. Please initialize the FPU before use."
 #endif
 
-uint8_t SOSpole[32] = {1, 0, 1, 0, 1, 0, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0};
-uint32_t SOSbinNum = 0b10101001110111011100101010000000;
+
+// UKOL 1:  Rozsviceni led pomoci tlacitka skrze interrupt Handler
+
+//// Definici handleru zde "prepiseme" handlery inicializujici se ve startup kodu
+//void EXTI0_1_IRQHandler(void) {
+//	if (EXTI->PR & EXTI_PR_PR0) { // check line 0 has triggered the IT
+//		EXTI->PR |= EXTI_PR_PR0; // clear the pending bit
+////		Tohle je negovani stavu LED2 v ODR - output data register
+//		GPIOB->ODR ^=(1<<0);
+//	}
+//}
+
+
+// UKOL 2 Rozblikani LED pomoci casovace Systick - Systick handleru a vlastni blikaci funkce
+
+	volatile uint32_t Tick;
+
+	void SysTick_Handler(void) {
+		Tick++;
+	}
+
+
+	#define LED_TIME_BLINK (300)
+
+	void blikac(void) {
+		static uint32_t delay;
+
+		if (Tick > delay + LED_TIME_BLINK) {
+			GPIOA->ODR  ^=(1<<4);
+			delay = Tick;
+		}
+	}
+
+// UKOL 3 rozsviceni LED1 a 2 na zaklade stisku tlacitek 1 a 2.
+
+#define BUTTON_READ_TIME (40)
+#define LED_TIME_SHORT (100)
+#define LED_TIME_LONG (1000)
+
+void tlacitka(void) {
+
+	static uint32_t lastButtonRead;
+	static uint32_t old_s1;
+	static uint32_t old_s2;
+	static uint32_t off_time;
+	uint32_t new_s1 = GPIOC->IDR & (1<<1);
+	uint32_t new_s2 = GPIOC->IDR & (1<<0);
+
+	if (Tick > (lastButtonRead + BUTTON_READ_TIME)){
+
+		if (old_s1 && !new_s1) { // falling edge
+			off_time = Tick + LED_TIME_LONG;
+			GPIOB->BSRR = (1<<0);
+		}
+
+		old_s1 = new_s1;
+
+		if (old_s2 && !new_s2) { // falling edge
+			off_time = Tick + LED_TIME_SHORT;
+			GPIOB->BSRR = (1<<0);
+		}
+
+		old_s2 = new_s2;
+
+
+		lastButtonRead = Tick;
+	}
+	if (Tick > off_time) {
+		GPIOB->BRR = (1<<0);
+	}
+
+}
+
+
 
 int main(void)
 {
-	RCC->AHBENR |= RCC_AHBENR_GPIOAEN;
-	GPIOA->MODER |= GPIO_MODER_MODER5_0;
+	//	inicializace casovace systick s periodou 1ms na zaklade systemovych hodin s kmitoctem 8 MHz
+	SysTick_Config(8000); // 1ms
 
-//	GPIOA->BSRR = (1<<5); // set
-//	GPIOA->BRR = (1<<5); // reset
-//	GPIOA->ODR ^= (1<<5); // toggle
+	RCC->AHBENR |= RCC_AHBENR_GPIOAEN | RCC_AHBENR_GPIOBEN | RCC_AHBENR_GPIOCEN; // enable
+	GPIOA->MODER |= GPIO_MODER_MODER4_0; // LED1 = PA4, output
+	GPIOB->MODER |= GPIO_MODER_MODER0_0;  // LED2 = PB0, output
+	GPIOC->PUPDR |= GPIO_PUPDR_PUPDR0_0; // S2 = PC0, pullup
+	GPIOC->PUPDR |= GPIO_PUPDR_PUPDR1_0; // S1 = PC1, pullup
+
+//	Struktury pro externi preruseni
+//	RCC->APB2ENR |= RCC_APB2ENR_SYSCFGEN;
+////	Systick najdeme vsude, nehlede na vyrobci desky
+//	SYSCFG->EXTICR[0] |= SYSCFG_EXTICR1_EXTI0_PC; // select PC0 for EXTI0
+//	EXTI->IMR |= EXTI_IMR_MR0; // mask
+//	EXTI->FTSR |= EXTI_FTSR_TR0; // trigger on falling edge
+//	NVIC_EnableIRQ(EXTI0_1_IRQn); // enable EXTI0_1
+
+
+//Struktury pro obsluhu tlacitek
+
+
+
+
 
     /* Loop forever */
 	for(;;){
-		//Verze s prochazenim pole iteraci
-//		for(uint8_t i = 0; i < 32; i++){
-//			if (SOSpole[i]){
-//
-//				GPIOA->BSRR = (1<<5); // set
-//			}
-//			else {
-//				GPIOA->BRR = (1<<5); // reset
-//			}
-//			for (volatile uint32_t i = 0; i < 120000; i++) {
-//			}
-//		}
-		//Verze s prochazenim binarniho cisla
-//		for(uint8_t i = 0; i < 32; i++){
-//			if ((SOSbinNum & (1<<i) )){
-//				GPIOA->BSRR = (1<<5); // set
-//			}
-//			else {
-//				GPIOA->BRR = (1<<5); // reset
-//			}
-//			for (volatile uint32_t i = 0; i < 120000; i++) {
-//			}
-//		}
-		//Optimalizovana verze - pry bit shift o promenou neni pro procesor moc cool
-		uint32_t mask = 0x80000000;
-		for(uint8_t i = 0; i < 32; i++){
-			if (SOSbinNum & mask){
-				GPIOA->BSRR = (1<<5); // set
-			}
-			else {
-				GPIOA->BRR = (1<<5); // reset
-			}
-			mask = mask>>1;
-			for (volatile uint32_t i = 0; i < 120000; i++) {
-			}
-		}
+//		blikac();
+		tlacitka();
+
 	}
 }
 
+//Fun note - promena Tick systemu systick ma kapacitu kolem 4 miliard milisekund - to je cca 48 dni - pak pretece zpet na nulu
