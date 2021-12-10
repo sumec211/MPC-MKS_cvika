b2c503f70fd5a24613b8f693d55aa805fb7518ed
Stanislav <sumec211@seznam.cz>
2021-12-10 08:51:04 +0100

    Dokoncene ukoly s CMSIS knihovnou

---
+++ CV03/Debug/Src/sct.d
@@ -0,0 +1,19 @@
+Src/sct.o: ../Src/sct.c ../CMSIS/Device/ST/STM32F0xx/Include/stm32f0xx.h \
+ ../CMSIS/Device/ST/STM32F0xx/Include/stm32f030x8.h \
+ ../CMSIS/Include/core_cm0.h ../CMSIS/Include/core_cmInstr.h \
+ ../CMSIS/Include/core_cmFunc.h \
+ ../CMSIS/Device/ST/STM32F0xx/Include/system_stm32f0xx.h ../Src/sct.h
+
+../CMSIS/Device/ST/STM32F0xx/Include/stm32f0xx.h:
+
+../CMSIS/Device/ST/STM32F0xx/Include/stm32f030x8.h:
+
+../CMSIS/Include/core_cm0.h:
+
+../CMSIS/Include/core_cmInstr.h:
+
+../CMSIS/Include/core_cmFunc.h:
+
+../CMSIS/Device/ST/STM32F0xx/Include/system_stm32f0xx.h:
+
+../Src/sct.h:
