f422fbf8abf78fb1f11cd6857f851b47da49b8e7
Stanislav <sumec211@seznam.cz>
2021-12-10 08:53:48 +0100

    Push kvuli kolizi repozitaru

---
+++ CV03HAL/Core/Src/sct.h
@@ -0,0 +1,17 @@
+/*
+ * sct.h
+ *
+ *  Created on: Dec 8, 2021
+ *      Author: ssind
+ */
+
+
+#ifndef SCT_H_
+#define SCT_H_
+
+void sct_init(void);
+void sct_led(uint32_t value);
+void sct_value(uint16_t value);
+
+#endif /* SCT_H_ */
+
